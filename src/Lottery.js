1564388df76685cf3b00aa5d99ad6d6372dca98e
kasimugur <kasiugur7@gmail.com>
2024-05-20 18:38:21 +0300

    updated component

--- src/Lottery.js
+++ src/Lottery.js
@@ -1,11 +1,40 @@
 import React, {Component} from "react";
+import Ball from "./Ball";
 
 class Lottery extends Component {
+  static defaultProps = {
+    title: 'şans oyunu',
+    maxBalls:6,
+    maxNum:40,
+  }
+  constructor(props){
+    super(props)
+    this.state = {
+      nums: Array.from({length: this.props.maxBalls})
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+generete() {
+  this.setState((currState) => ({
+    nums: currState.nums.map(
+      (n) => Math.floor(Math.random() * this.props.maxNum ) +1 )
+  }))
+}
+
+handleClick()  {
+  this.generete()
+}
   render() {
     return(<>
+    <section>
+      <h1>{this.props.title} </h1>
     <div>
-      <h1>şans oyunu</h1>
-    </div>
+      {this.state.nums.map((n) => (
+        <Ball num={n} />
+      ))}
+      </div>
+      <button onClick={this.handleClick}>günün şanslı sayıları</button>
+    </section>
     </>)
   }
 }
